692ac994477bae82ab5b8cd5900e1e3dd26f5b57
kimsy <duddl2102@naver.com>
2020-12-14 17:30:01 +0900

    미니 프로젝트 커밋

--- src/com/javaex/ex01/NumberApp.java
+++ src/com/javaex/ex01/NumberApp.java
@@ -109,6 +109,7 @@ public class NumberApp {
 				}
 				
 				System.out.println("[등록되었습니다.]");
+				System.out.println("");
 				bw.close();
 			} else if (num == 3) {	//3번을 입력시 원하는 번호 삭제
 				nList.clear();
@@ -146,12 +147,42 @@ public class NumberApp {
 				}
 				
 				System.out.println("삭제되었습니다.");
+				System.out.println("");
 				br.close();
 				bw.close();
-			} else if(num == 4) {
+			} else if(num == 4) {	//4번을 입력하면 해당 단어가 이름에 포함되있는 사람을 검색
+				System.out.println("<4. 검색>");
+				nList.clear();
+				Reader rd = new FileReader("C:\\javaStudy\\미니프로젝트\\PhoneDB.txt");
+				BufferedReader br = new BufferedReader(rd);
 				
+				while(true) {
+					String str = br.readLine();
+					
+					if(str == null) {
+						break;
+					}
+					
+					String[] strarr = str.split(",");
+					Number n05 = new Number(strarr[0], strarr[1], strarr[2]);
+					nList.add(n05);
+				}
+				
+				System.out.print(">> 이름 : ");
+				String str = sc.next();
+				
+				for(int i = 0; i < nList.size(); i++) {
+					//indexof 함수로 해당 문자열이 있는지 찾아본다. (-1이 나오면 없는것)
+					if((nList.get(i).getName().indexOf(str)) != -1) {
+						System.out.println((i+1) + ".\t" + nList.get(i).getName() +
+								"\t" + nList.get(i).getHp() + "\t" + nList.get(i).getCompany());
+					}
+				}
+				
+				br.close();
 			} else {
 				System.out.println("[다시 입력해 주세요.]");
+				System.out.println("");
 			}
 		}	
 		sc.close();
